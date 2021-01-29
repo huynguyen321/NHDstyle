bc0ad20df2c6d721171401e8d2dec139c6c98879
Dang Van Duc <duc.dang22@pn.com.vn>
2021-01-29 08:21:15 +0700

    Service Page

---
+++ ServicePage/ServicePage.js
@@ -0,0 +1,16 @@
+<div class="py-5 container">
+        <h3 class="font-weight-bold mb-0">Hiệu Ứng Hover 5</h3>
+        <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <div class="row">
+            <div class="col-12 mb-3 mb-lg-0">
+                <div class="hover hover-5 text-white rounded"><img src="https://res.cloudinary.com/dn4nxz7f0/image/upload/v1586172675/Bootstrap%20hover%20hinh%20anh/Hover_Image_Bootstrap_Demo_1_eb5hk5.jpg" alt="">
+                    <div class="hover-overlay"></div>
+                    <div class="hover-5-noidung px-5 py-4">
+                        <h3 class="hover-5-tieude text-uppercase font-weight-bold mb-0"> <span class="font-weight-light">Tiêu Đề </span>Hình Ảnh</h3>
+                        <p class="hover-5-trichdan font-weight-light mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 mb-3 mb-lg-0"></div>
+        </div>
+    </div>
